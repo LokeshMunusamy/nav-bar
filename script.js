94b323b2fb927e0ee66c24e824bd87b75cf6ab0e
LokeshMunusamy <lokeshmdckap@gmail.com>
2025-01-16 14:03:54 +0530

    Rework

--- script.js
+++ script.js
@@ -3,31 +3,48 @@ let visible=document.getElementsByClassName('menu')[0];
 let details=document.querySelectorAll('.details');
 let icon=document.querySelectorAll('.icon');
 let xmark=document.getElementById('xmark');
+let bod=document.getElementsByTagName('body');
 
 menuBar.addEventListener("click",()=>{
     visible.style.visibility = 'visible';
 });
 
-details.forEach((detail)=>{
+
+// bod.addEventListener("click",()=>{
+//     // visible.style.visibility = 'hidden';
+    
+    
+// });
+
+details.forEach(detail=>{
     detail.addEventListener('mouseover',()=>{
         detail.style.backgroundColor='lightblue';
+        detail.style.cursor='pointer';
+        detail.style.scale=1.2;
     });
     detail.addEventListener('mouseout',()=>{
         detail.style.backgroundColor='';
+        detail.style.scale='';
     });
+
 });
-icon.forEach((icons)=>{
+icon.forEach(icons=>{
     icons.addEventListener('mouseover',()=>{
         icons.style.color='rgb(5, 59, 109)';
+        icons.style.cursor='pointer';
+        icons.style.scale=1.2;
     });
     icons.addEventListener('mouseout',()=>{
         icons.style.color='';
+        icons.style.scale='';
     });
 });
 xmark.addEventListener('mouseover',()=>{
     xmark.style.transform='rotate(360deg)';
+    xmark.style.cursor='pointer';
 });
 xmark.addEventListener('click',()=>{
     visible.style.visibility = 'hidden';
 });
+
 
